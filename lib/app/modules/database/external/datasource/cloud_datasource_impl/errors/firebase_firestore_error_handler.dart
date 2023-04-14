8a91c3390aafbf7e82aff4f6d0f758e4f9fb3727
alefealvessilva <alefeasc@gmail.com>
2023-04-14 20:30:29 -0300

    improve firestore error handler

--- lib/app/modules/database/external/datasource/cloud_datasource_impl/errors/firebase_firestore_error_handler.dart
+++ lib/app/modules/database/external/datasource/cloud_datasource_impl/errors/firebase_firestore_error_handler.dart
@@ -3,48 +3,75 @@ import 'package:verify/app/core/register_log.dart';
 import 'package:verify/app/core/send_logs_to_web.dart';
 
 enum FirebaseErrorType {
-  docummentNotFound(
-    errorCode: 'invalid-argument',
-    message: 'O Documento solicitado não exite',
-  ),
-  emptyDocummentData(
-    errorCode: 'empty-documment-data',
-    message: 'Não existe dados para a solicitacao',
+  cancelled(
+    errorCode: 'cancelled',
+    message: 'A operação foi cancelada pelo usuário',
   ),
   invalidArgument(
     errorCode: 'invalid-argument',
-    message: 'O valor fornecido é inválido',
-  ),
-  failedPrecondition(
-    errorCode: 'failed-precondition',
     message:
-        'Não foi possível realizar a operação porque não foi atendida uma condição necessária',
+        'O valor inserido não é válido. Por favor, verifique as informações fornecidas e tente novamente.',
   ),
-  unauthenticated(
-    errorCode: 'failed-precondition',
+  deadlineExceeded(
+    errorCode: 'deadline-exceeded',
     message:
-        'Não foi possível realizar a operação porque o usuário não está autenticado.',
-  ),
-  permissionDenied(
-    errorCode: 'permission-denied',
-    message:
-        'Não foi possível realizar a operação devido à falta de permissões.',
+        ' A operação levou mais tempo do que o esperado para ser concluída. Por favor, tente novamente mais tarde.',
   ),
   notFound(
     errorCode: 'not-found',
     message:
-        'Não foi possível concluir a operação porque o recurso solicitado não foi encontrado.',
+        ' O documento ou recurso solicitado não foi encontrado. Verifique as informações fornecidas e tente novamente.',
   ),
-  aborted(
-    errorCode: 'aborted',
-    message: 'A operação foi cancelada.',
-  ),
-
   alreadyExists(
     errorCode: 'already-exists',
-    message: 'Não foi possível realizar a operação porque o recurso já existe.',
+    message:
+        ' O documento ou recurso que você está tentando criar já existe. Por favor, verifique as informações fornecidas e tente novamente.',
   ),
-
+  permissionDenied(
+    errorCode: 'permission-denied',
+    message:
+        'Você não tem permissão para acessar o documento ou recurso solicitado. Por favor, entre em contato com o suporte para mais informações',
+  ),
+  resourceExhausted(
+    errorCode: 'resource-exhausted',
+    message:
+        'O número de solicitações excedeu o limite permitido. Por favor, tente novamente mais tarde.',
+  ),
+  failedPrecondition(
+    errorCode: 'failed-precondition',
+    message:
+        'A operação não pode ser realizada devido ao estado atual do recurso. Por favor, verifique as informações fornecidas e tente novamente.',
+  ),
+  aborted(
+    errorCode: 'failed-precondition',
+    message:
+        'A operação foi interrompida devido a um conflito de concorrência. Por favor, tente novamente mais tarde.',
+  ),
+  outOfRange(
+    errorCode: 'out-of-range',
+    message:
+        'O valor inserido está fora do intervalo permitido. Por favor, verifique as informações fornecidas e tente novamente.',
+  ),
+  internal(
+    errorCode: 'internal',
+    message:
+        'Ocorreu um erro interno no servidor. Por favor, tente novamente mais tarde.',
+  ),
+  unavailable(
+    errorCode: 'unavailable',
+    message:
+        'O serviço não está disponível no momento. Por favor, tente novamente mais tarde',
+  ),
+  dataLoss(
+    errorCode: 'data-loss',
+    message:
+        'Houve uma perda de dados durante a transmissão. Por favor, tente novamente mais tarde.',
+  ),
+  unauthenticated(
+    errorCode: 'unauthenticated',
+    message:
+        'Você precisa estar autenticado para acessar o recurso solicitado. Por favor, faça o login e tente novamente.',
+  ),
   unknown(
     errorCode: 'unknown',
     message:
